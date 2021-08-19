6c11dd4ef9dcb25333ea773a6d94eb9051a966b8
osmanmm <mohamed.osman@outlook.com>
2021-08-19 16:23:37 +0000

    added test folder

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-    if(actual === expected){
-        console.log("All matched. Test case passed");
-    } else{
-        console.log("Not matched. Test case not passed");
-    }
-  
-}
-
+const assertEqual = require('./assertEqual');
 const head = function(array){
     return array[0];
 }
