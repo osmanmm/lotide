6578608c8935b984ae9d18b24c706309a2f979fd
osmanmm <mohamed.osman@outlook.com>
2021-08-19 16:47:06 +0000

    added middelTest file

--- middle.js
+++ middle.js
@@ -1,14 +1,6 @@
+const assertEqual = require('./assertEqual');
 const eqArrays = require("./eqArrays");
-const x = String.fromCodePoint(0x1F44D);
-  const y = String.fromCodePoint(0x1F44E);
-const assertArraysEqual = function(array1, array2) {
-    if (eqArrays(array1, array2)) {
-      console.log(` ${x} Assertion passed: ${array1} === ${array2}`);
-    } else {
-      console.log(`${y}  Assertion failed: ${array1} !== ${array2}`);
-      assertEqual(essertArraysEqual(["1", "2", "3"], ["1", "2", 3]), true);
-    }
-  };
+
   const middle = function(array) {
     let array_result = []; 
     if (array.length <= 2 && array.length > 0) {
--- tail.js
+++ tail.js
@@ -2,9 +2,6 @@ const assertEqual = require('./assertEqual');
   const tail = function(arr){
     return arr.slice(1);
   }
-  
-  //assertEqual(1,1);
-//   assertEqual("Lighthouse", "Labs");
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 
 assertEqual(result, ["Lighthouse", "Labs"]);
