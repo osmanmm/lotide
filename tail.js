42bce2382371a10d2e438d4e67a9e900a2ba66de
osmanmm <mohamed.osman@outlook.com>
2021-08-20 16:01:31 +0000

    modified tail file to use mocah

--- tail.js
+++ tail.js
@@ -2,9 +2,9 @@ const assertEqual = require('./assertEqual');
   const tail = function(arr){
     return arr.slice(1);
   }
-const result = tail(["Hello", "Lighthouse", "Labs"]);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-assertEqual(result, ["Lighthouse", "Labs"]);
-console.log(result);
+// assertEqual(result, ["Lighthouse", "Labs"]);
+// console.log(result);
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,1 +1,15 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+describe("#tail", () => {
+    it("returns Lighthouse, Labs for [Hello, Lighthouse, Labs]", () => {
+        assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+    });
+    it("returns 1,2 for [1, 2, 3]", () => {
+        assert.deepEqual(tail([1, 2, 3]), [2,3]);
+      });
+      it("returns [] for [Lighthouse]", () => {
+        assert.deepEqual(tail(["Lighthouse"]), []);
+    });
+});
