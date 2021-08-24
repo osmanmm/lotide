9e2a90033e1423b119ffe9cf088017eb30184529
osmanmm <mohamed.osman@outlook.com>
2021-08-24 15:56:34 +0000

    added eqObject to top of assertEqual file , remove unused code from without file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,21 +1,4 @@
-const eqObjects = function (object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  } else {
-    for (let i in object1) {
-      if ((Array.isArray(object1[i]) && Array.isArray(object2[i]))) {
-        if (eqArrays(object1[i], object2[i]) === false) {
-          return false
-        }
-      } else {
-        if (object1[i] !== object2[i]) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-}
+const eqArrays = require("./eqObjects");
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; 
   if (eqObjects(actual, expected)) {
--- without.js
+++ without.js
@@ -1,13 +1,3 @@
-// const without = function(source, itemsToRemove) {
-//     for (let i = 0; i < source.length; i++) {
-//       for (let j = 0; j < itemsToRemove.length; j++) {
-//         if (source[i] === itemsToRemove[j]) {
-//           source.splice(i, 1);
-//         }
-//       }
-//     }
-//     return source;
-//   };
 const without = function(array1, array2) {
   let filteredArray = [];
   for (const element of array1) {
